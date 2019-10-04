2ae0cff28c601f7609896232a7121c93fd2d7464
Amtul Nazneen <52686715+amtul-nazneen@users.noreply.github.com>
2019-10-04 12:01:54 -0500

    Update Test.java

--- src/app/test/Test.java
+++ src/app/test/Test.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 public class Test {
 	public static ArrayList<String> getActiveProfilesTest() {
 		ArrayList<String> activeTlProfile = new ArrayList<String>();
-
-		// activeTlProfile.add("5627");
 		activeTlProfile.add("1133");
 		return activeTlProfile;
 	}
